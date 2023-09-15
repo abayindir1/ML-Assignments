973f39d07809762dee4b1234adc98a56cc05c11a
Abdullah Bayindir <byndrabdullah@gmail.com>
2023-09-15 14:39:11 -0500

    handled the sklearn part of the project. there are mse value inconsistencies.

--- Assignment-1/part1.py
+++ Assignment-1/part1.py
@@ -10,21 +10,21 @@ x3 = data["X3 distance to the nearest MRT station"].values
 x4 = data["X4 number of convenience stores"].values
 y= data["Y house price of unit area"].values
 
-print(x1)
+# print(x1)
 
 # the gradient for my situation
 def ssr_gradient(x, y, w):
-    res = w[0] + w[1] * x[0] + w[2] * x[1] + w[3] * x[2] + w[4] * x[3] - y
+    res = w[0] + w[1] * x[:, 0] + w[2] * x[:, 1] + w[3] * x[:, 2] + w[4] * x[:, 3] - y
     gradient_w0 = res.mean()
-    gradient_w1 = (res * x[0]).mean()
-    gradient_w2 = (res * x[1]).mean()
-    gradient_w3 = (res * x[2]).mean()
-    gradient_w4 = (res * x[3]).mean()
+    gradient_w1 = (res * x[:, 0]).mean()
+    gradient_w2 = (res * x[:, 1]).mean()
+    gradient_w3 = (res * x[:, 2]).mean()
+    gradient_w4 = (res * x[:, 3]).mean()
     return gradient_w0, gradient_w1, gradient_w2, gradient_w3, gradient_w4
 
 # generic method to minimize ANY convex function in the world
 def gradient_descent(
-     gradient, x, y, start, learn_rate=0.1, n_iter=50, tolerance=1e-06
+     gradient, x, y, start, learn_rate=0.00015, n_iter=50, tolerance=1e-06
  ):
   vector = start
   for _ in range(n_iter):
@@ -35,9 +35,22 @@ def gradient_descent(
   return vector
 
 # initial w values
-initial_w = np.array(0.5, 0.5, 0.5, 0.5, 0.5)
+initial_w = np.array([.5,.8,.4,.6,.6])
 
 # training the model
-train = gradient_descent(ssr_gradient, np.column_stack((x1, x2, x3, x4)), y, start=initial_w)
+trained = gradient_descent(ssr_gradient, np.column_stack((x1, x2, x3, x4)), y, start=initial_w)
+# print(trained)
 
-print(train)
+# extracting the test data
+test_data = pd.read_excel("test-data.xlsx")
+test_x1 = data["X1 transaction date"].values
+test_x2 = data["X2 house age"].values
+test_x3 = data["X3 distance to the nearest MRT station"].values
+test_x4 = data["X4 number of convenience stores"].values
+test_y= data["Y house price of unit area"].values
+
+# getting mse value
+prediction = test_predictions = trained[0] + trained[1] * test_x1 + trained[2] * test_x2 + trained[3] * test_x3 + trained[4] * test_x4
+# print(prediction)
+test_mse = ((y - prediction) ** 2).mean()
+print(f"mean squared error on the test data: {test_mse}")
---
+++ Assignment-1/part2.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import sklearn.linear_model as sklm
+import sklearn.metrics as skm
+
+# extracting data from training file
+data = pd.read_excel("training.xlsx")
+# x1 = data["X1 transaction date"].values
+# x2 = data["X2 house age"].values
+# x3 = data["X3 distance to the nearest MRT station"].values
+# x4 = data["X4 number of convenience stores"].values
+y = data["Y house price of unit area"].values
+x=data[["X1 transaction date", "X2 house age", "X3 distance to the nearest MRT station", "X4 number of convenience stores"]]
+
+# declaring linear regression model and training it
+model = sklm.LinearRegression()
+model.fit(x, y)
+
+# extracting testing data from test data file 
+test_data = pd.read_excel("test-data.xlsx")
+# test_x1 = data["X1 transaction date"].values
+# test_x2 = data["X2 house age"].values
+# test_x3 = data["X3 distance to the nearest MRT station"].values
+# test_x4 = data["X4 number of convenience stores"].values
+test_y= data["Y house price of unit area"].values
+test_x=data[["X1 transaction date", "X2 house age", "X3 distance to the nearest MRT station", "X4 number of convenience stores"]]
+
+prediction = model.predict(test_x)
+
+mse = skm.mean_squared_error(test_y, prediction)
+
+print(mse)
